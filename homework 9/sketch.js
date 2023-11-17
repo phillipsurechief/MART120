416277bb1f4623a02b64578f18b9b1d788ad5ab7
phillipsurechief <143650287+phillipsurechief@users.noreply.github.com>
2023-11-17 13:50:57 -0700

    hi

---
+++ homework 9/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(800,700);
+  }
+  
+  function draw() {
+    background(220);
+    ellipse(400,300,400,470)
+    square(400,150,90);
+    square(250,150,90);
+    rect(340,180,60,20);
+    rect(490,180,90,20);
+    rect(200,180,50,20);
+    triangle(300, 290, 370, 200, 400, 290);
+    line(300, 450, 450, 450);
+    line(300, 450, 270,400);
+    line(270, 400, 500, 400);
+    line(450, 450, 500,400);
+    ellipse(390,100,210,70);
+    textSize(32);
+    text('Self Portrait', 300, 30);
+    textSize(50);
+    text('Phillip Surechief', 250, 600)
+    Stokeweight(30)
+    Point(400, 500)
+  }
